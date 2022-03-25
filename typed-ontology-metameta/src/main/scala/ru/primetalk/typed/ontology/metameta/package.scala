84aa687cb7ced51e270705cab325b9ccaed13505
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2022-03-26 00:06:14 +0300

    OntologyType

--- typed-ontology-metameta/src/main/scala/ru/primetalk/typed/ontology/metameta/package.scala
+++ typed-ontology-metameta/src/main/scala/ru/primetalk/typed/ontology/metameta/package.scala
@@ -5,16 +5,17 @@ import scala.language.{higherKinds, implicitConversions}
 /**
   * Meta-meta package contains instruments to define meta level of ontology.
   *
-  * For instance here we define type classes that allows us to use arbitrary types as property identifiers.
+  * For instance here we define type classes that allows us to use arbitrary 
+  * types as property identifiers.
   */
-sealed trait AttributeType
+sealed trait OntologyType
 /** Phantom type that represents a record. */
-abstract final class Record[+A] extends AttributeType
+abstract final class Record[+A] extends OntologyType
 /** A type that represents a simple Scala type that is supported. */
-abstract final class Scalar[A] extends AttributeType
+abstract final class Scalar[A] extends OntologyType
 
 /** Phantom type that represents a collection of elements of type A. */
-abstract final class MetaSeq[+A <: AttributeType] extends AttributeType
+abstract final class MetaSeq[+A <: OntologyType] extends OntologyType
 
-trait PropertyIdTypeClass[PropertyId[-_,_]]:
-  def name[A,B](p: PropertyId[A,B]): String
+trait PropertyIdTypeClass[PropertyId[-_,_ <: OntologyType]]:
+  def name[A, B <: OntologyType](p: PropertyId[A,B]): String
