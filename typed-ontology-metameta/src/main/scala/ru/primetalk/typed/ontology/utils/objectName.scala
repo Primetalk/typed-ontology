4233c82f976c9f35ce690998b86e04752b69205b
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2022-04-06 22:36:16 +0300

    Utility objectName

---
+++ typed-ontology-metameta/src/main/scala/ru/primetalk/typed/ontology/utils/objectName.scala
@@ -0,0 +1,9 @@
+package ru.primetalk.typed.ontology.utils
+
+def objectName(o: Any): String =
+  val simpleName = o.getClass.getSimpleName
+  val l = simpleName.length
+  if simpleName(l - 1) == '$' then
+    simpleName.substring(0, l - 1)
+  else
+    simpleName
