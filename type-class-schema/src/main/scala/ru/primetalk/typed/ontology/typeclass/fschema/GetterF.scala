c89ed388f3665ac7f0528243ca49ec5db213db0e
SingletonV <singletonv@github.com>
2025-06-25 19:06:48 +0300

    Attemt to add F[_]

---
+++ type-class-schema/src/main/scala/ru/primetalk/typed/ontology/typeclass/fschema/GetterF.scala
@@ -0,0 +1,10 @@
+package ru.primetalk.typed.ontology.typeclass.fschema
+
+import ru.primetalk.typed.ontology.typeclass.schema.ValueWithSchema
+
+import scala.annotation.implicitNotFound
+
+@FunctionalInterface
+@implicitNotFound(msg = "Cannot obtain ${Column} of type ${ColumnValue} from ${Value}")
+trait GetterF[F[_], Column, ColumnValue, Value <: Tuple]:
+  def apply(rtc: Tuple.Map[Value, F]): F[ValueWithSchema[Column, ColumnValue]]
